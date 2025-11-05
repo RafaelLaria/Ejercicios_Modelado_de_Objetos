dc4366508b8abffa5a1c42e5580891704f947634
RafaelLaria <rafael.lariaortiz@usp.ceu.es>
2025-11-05 18:56:44 +0100

    clase edificio

---
+++ Clases/Ejercicio_4/edificio.py
@@ -0,0 +1,11 @@
+class Edicio:
+    def __init__(self, lugar, fecha_inicio, fecha_fin, fecha_BIC, material, estilo):
+        self.lugar = lugar
+        self.fecha_inicio = fecha_inicio
+        self.fecha_fin = fecha_fin
+        self.fecha_BIC = fecha_BIC
+        self.material = material
+        self.estilo = estilo
+    def __str__(self):
+        return f"{self.lugar, self.fecha_inicio, self.fecha_fin, self.fecha_BIC, self.material, self.estilo}"
+    
