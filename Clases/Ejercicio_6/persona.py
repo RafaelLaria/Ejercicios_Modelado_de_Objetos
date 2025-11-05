7e31349b5e01235b3f69d7ec98fa56f0cd892c94
RafaelLaria <rafael.lariaortiz@usp.ceu.es>
2025-11-05 20:21:45 +0100

    clase persona

---
+++ Clases/Ejercicio_6/persona.py
@@ -0,0 +1,9 @@
+class Persona:
+    def __init__(self, nombre, primer_apellido, segundo_apellido, fecha_nacimiento, numero_identificacion):
+        self-nombre = nombre
+        self.primer_apellido = primer_apellido
+        self.segundo_apellido = segundo_apellido
+        self.fecha_nacimiento = fecha_nacimiento
+        self.numero_identificacion = numero_identificacion
+    def __str__(self):
+        return f"{self.nombre, self.primer_apellido, self.segundo_apellido, self.fecha_nacimiento, self.numero_identificacion}"
