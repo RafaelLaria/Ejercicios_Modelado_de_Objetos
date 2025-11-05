a10c97a7c40a0f932f9a01482279815c1f6299fb
RafaelLaria <rafael.lariaortiz@usp.ceu.es>
2025-11-05 19:44:43 +0100

    ejercicio 4 a medias

--- Clases/Ejercicio_4/catedral.py
+++ Clases/Ejercicio_4/catedral.py
@@ -1,6 +1,6 @@
 from edificio import Edificio
 class Catedral(Edificio):
-    def __init__(self, lugar, fecha_inicio, fecha_fin, fecha_BIC, material, estilo, nombre = "Catedral de Santiago de Compostela", culto = "Católico", fecha_1consagracion = "1128", fecha_2inicio = "1168", fecha_2consagracion = "1211"):
+    def __init__(self, lugar, fecha_inicio, fecha_fin, fecha_BIC, material, estilo, nombre, culto, fecha_1consagracion, fecha_2inicio, fecha_2consagracion):
         super().__init__(lugar, fecha_inicio, fecha_fin, fecha_BIC, material, estilo)
         self.nombre = nombre
         self.culto = culto
@@ -8,4 +8,4 @@ class Catedral(Edificio):
         self.fecha_2inicio = fecha_2inicio
         self.fecha_2consagracion= fecha_2consagracion
     def __str__(self):
-        f"La {self.nombre} es un templo de culto {self.culto} situado en {self.lugar}.\nLa construcción se inició en {self.fecha_inicio} y se construyó fundamentalmente en {self.material}. Se terminó en {self.fecha_fin} y se consagró por primera vez en {self.fecha_1consagracion}.\nLa construcción se continuó en {self.fecha_2inicio} y se consgró definitivamente en {self.fecha_2consagracion}.\nFue declarada Bien de Interés Cultural en {self.fecha_BIC} y emplea múltiples estilos arquitectónicos: {self.estilo}"
+        return f"La {self.nombre} es un templo de culto {self.culto} situado en {self.lugar}.\nLa construcción se inició en {self.fecha_inicio} y se construyó fundamentalmente en {self.material}. Se terminó en {self.fecha_fin} y se consagró por primera vez en {self.fecha_1consagracion}.\nLa construcción se continuó en {self.fecha_2inicio} y se consagró definitivamente en {self.fecha_2consagracion}.\nFue declarada Bien de Interés Cultural en {self.fecha_BIC} y emplea múltiples estilos arquitectónicos: {self.estilo}"
--- Clases/Ejercicio_4/main.py
+++ Clases/Ejercicio_4/main.py
@@ -1,4 +1,4 @@
 from catedral import Catedral
 
-catedral = Catedral("1075", "granito", "1122", "1896" "románico, gótico, barroco, plateresco, neoclásico")
+catedral = Catedral("Catedral de Santiago de Compostela", "católico", "Santiago de Compostela, Galicia, España", "1075", "granito", "1122", "1128", "1168", "1211", "1896", "románico, gótico, barroco, plateresco, neoclásico")
 print(catedral)
