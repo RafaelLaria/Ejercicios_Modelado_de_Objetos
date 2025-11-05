f0ddd3e7efd0c27bc6aad0cd030ba73ba9a522fd
RafaelLaria <rafael.lariaortiz@usp.ceu.es>
2025-11-05 20:30:24 +0100

    cambio2

--- Clases/Ejercicio_6/persona.py
+++ Clases/Ejercicio_6/persona.py
@@ -6,4 +6,6 @@ class Persona:
         self.fecha_nacimiento = fecha_nacimiento
         self.numero_identificacion = numero_identificacion
     def __str__(self):
-        return f"{self.nombre, self.primer_apellido, self.segundo_apellido, self.fecha_nacimiento, self.numero_identificacion}"
+        return (f"Nombre y apellidos: {self.nombre} {self.primer_apellido} {self.segundo_apellido}\n"
+                f"Fecha de nacimiento : {self.fecha_nacimiento}\n"
+                f"Número de identificación: {self.numero_identificacion}")
