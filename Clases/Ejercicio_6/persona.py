aa5d99d63a38923bc998c29e6ef6382efa086d57
RafaelLaria <rafael.lariaortiz@usp.ceu.es>
2025-11-07 10:39:23 +0100

    cambio

--- Clases/Ejercicio_6/persona.py
+++ Clases/Ejercicio_6/persona.py
@@ -1,17 +1,22 @@
 class Persona:
     def __init__(self, nombre, primer_apellido, segundo_apellido, fecha_nacimiento, numero_identificacion):
-        self.nombre = str(input("Escriba el nombre"))
-        self.primer_apellido = str(input("Escriba el primer apellido"))
-        self.segundo_apellido = str(input("Escriba el segundo apellido"))
-        self.fecha_nacimiento = str(input("Escriba la fecha de nacimiento"))
-
-        while True:
-            num = str(input("Escriba el número de identificación"))
-            if len(num) == 9:
-                self.numero_identificacion = num
-            else:
-                print("ERROR: El número de identificación debe tener 9 caracteres")
+        self.nombre = nombre
+        self.primer_apellido = primer_apellido
+        self.segundo_apellido = segundo_apellido
+        self.fecha_nacimiento = fecha_nacimiento
+        self.numero_identificacion = numero_identificacion
     def __str__(self):
         return (f"Nombre y apellidos: {self.nombre} {self.primer_apellido} {self.segundo_apellido}\n"
                 f"Fecha de nacimiento : {self.fecha_nacimiento}\n"
                 f"Número de identificación: {self.numero_identificacion}")
+    def dar_nombre():
+        nombre = str(input("Escriba el nombre"))
+        return print(nombre)
+    def dar_apellidos():
+        primer_apellido = str(input("Escriba el primer apellido"))
+        segundo_apellido = str(input("Escriba el segundo apellido"))
+        return primer_apellido, segundo_apellido
+    def dar_fecha():
+        fecha_nacimiento = str(input("Escriba la fecha de nacimiento"))
+        return fecha_nacimiento
+    
