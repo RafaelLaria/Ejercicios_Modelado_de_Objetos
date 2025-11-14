6fe48304ea27732b7f431de85f6d1cde60a15697
RafaelLaria <rafael.lariaortiz@usp.ceu.es>
2025-11-14 10:35:41 +0100

    cambio

--- Clases/Ejercicio_6/main.py
+++ Clases/Ejercicio_6/main.py
@@ -1,4 +1,10 @@
 from persona import Persona
 
-persona = Persona()
+persona = Persona(nombre, primer_apellido, segundo_apellido, fecha_nacimiento, sexo, numero_identificacion)
+nombre = persona.dar_nombre()
+primer_apellido, segundo_apellido = persona.dar_apellidos()
+fecha_nacimiento = persona.dar_fecha()
+sexo = persona.dar_sexo()
+numero_identificacion = persona.dar_numero()
+
 print(persona)
