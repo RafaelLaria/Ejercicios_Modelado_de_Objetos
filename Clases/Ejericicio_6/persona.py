81f6bb6b3255bec7bea95965f9ee8cf0303bde03
RafaelLaria <rafael.lariaortiz@usp.ceu.es>
2025-11-18 19:26:41 +0100

    cambio

--- Clases/Ejericicio_6/persona.py
+++ Clases/Ejericicio_6/persona.py
@@ -1,11 +1,11 @@
 class Persona:
-    def __init__(self, nombre, primer_apellido, segundo_apellido, fecha_nacimiento, sexo, numero_identificacion):
-        self.nombre = nombre
-        self.primer_apellido = primer_apellido
-        self.segundo_apellido = segundo_apellido
-        self.fecha_nacimiento = fecha_nacimiento
-        self.sexo = sexo
-        self.numero_identificacion = numero_identificacion
+    def __init__(self):
+        self.nombre = ""
+        self.primer_apellido = ""
+        self.segundo_apellido = ""
+        self.fecha_nacimiento = ""
+        self.sexo = ""
+        self.numero_identificacion = ""
     def __str__(self):
         return f'NOMBRE: {self.nombre}\n' \
                f'APELLIDO 1: {self.primer_apellido}\n' \
